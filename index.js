a37361d7b4186924d7ad80ae1f5545d0ef480cc9
nacho.kawakami <nacho.kawakami@karabiner.tech>
2024-09-14 22:55:25 +0900

    削除機能、完了機能

--- index.js
+++ index.js
@@ -7,26 +7,48 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 if (todos) {
     todos.forEach(todo => {
         add(todo);
-    })
+    });
 }
 
 form.addEventListener("submit", function(event) {
     event.preventDefault(); //デフォルトのイベントを発生しないように中断できる
     add(); //画面に追加
-    clearInput(); //プレースホルダーを空に
 });
 
 function add(todo) {
     let todoText = input.value.trim();
+
     if (todo) {
-        todoText = todo;
+        todoText = todo.text;
     }
-    if (todoText.length > 0 && !isDuplicate(todoText)) { // 重複チェックを追加
+
+    if (todoText && !isDuplicate(todoText)) { // 重複チェックを追加
         const li = document.createElement("li");
+
         li.innerText = todoText;
         li.classList.add("list-group-item"); //HTMLのliにclassを追加
+
+        if (todo && todo.completed) {
+            li.classList.add("text-decoration-line-through");
+        }
+
+        //クリックで完了（打ち消し線）
+        li.addEventListener("click", function() {
+            li.classList.toggle("text-decoration-line-through");
+            saveData();
+        });
+
+        //右クリックで削除（スマホなら長押し）
+        li.addEventListener("contextmenu", function (event) { //contextmenu で右クリック検知
+            event.preventDefault(); // 右クリックのデフォルトイベントをブロック
+            if (confirm("本当に削除しますか？")) {
+                li.remove();
+                saveData();
+            }
+        });
+
         ul.appendChild(li); //ulのこどもとしてliを追加
-        todoText = "";
+        input.value = "";
         saveData(); //画面リロードしても消えないように保存
     }
 }
@@ -44,12 +66,13 @@ function isDuplicate(todoText) {
 function saveData() {
     const lists = document.querySelectorAll("li");
     let todos = [];
-    lists.forEach(list => {
-        todos.push(list.innerText);
-    });
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
 
-function clearInput() {
-    input.value = "";
+    lists.forEach((li) => {
+        todos.push({
+            text: li.innerText,
+            completed: li.classList.contains("text-decoration-line-through"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
