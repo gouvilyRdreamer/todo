7db00e1a8fa3f89e86ba24a426a8d5e1c3f7c452
nacho.kawakami <nacho.kawakami@karabiner.tech>
2024-09-14 22:03:17 +0900

    直前と同じ内容もtodo登録させない

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ form.addEventListener("submit", function(event) {
 });
 
 function add() {
-    let todoText = input.value;
-    if (todoText.length > 0) {
+    let todoText = input.value.trim();
+    if (todoText.length > 0 && !isDuplicate(todoText)) { // 重複チェックを追加
         const li = document.createElement("li");
         li.innerText = todoText;
         li.classList.add("list-group-item"); //HTMLのliにclassを追加
         ul.appendChild(li); //ulのこどもとしてliを追加
         todoText = "";
     }
+}
+
+function isDuplicate(todoText) {
+    const items = ul.getElementsByTagName("li");
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].innerText === todoText) {
+            return true; // 重複している場合
+        }
+    }
+    return false; // 重複していない場合
 }
